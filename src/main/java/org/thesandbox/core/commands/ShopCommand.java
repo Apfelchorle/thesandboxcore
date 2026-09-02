406b51f98f886305a1df29cc4ea1d7f987edd2a6
Apfelchorle <apfelchorle@gmail.com>
2026-09-02 15:00:26 +0100

    fix grappling hook location

--- src/main/java/org/thesandbox/core/commands/ShopCommand.java
+++ src/main/java/org/thesandbox/core/commands/ShopCommand.java
@@ -146,7 +146,7 @@ public class ShopCommand implements Listener,ISubCommand {
 
         // as shown above the Inventory Menu is 9 * 3 which is 27 slots
 
-        shopSlots.put(5, grapplingHookItem);
+        shopSlots.put(4, grapplingHookItem);
         inventory.setItem(5, grapplingHookItem.create());
 
         shopSlots.put(11, lightningRodItem);
