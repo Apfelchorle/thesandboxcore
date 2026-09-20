47373a6e0c80242a5599963864079fb6e8fa754f
Apfelchorle <apfelchorle@gmail.com>
2026-09-20 20:44:39 +0100

    changes to playerdatalistener
    Signed-off-by: Apfelchorle <apfelchorle@gmail.com>

--- src/main/java/org/thesandbox/core/commands/misc/chatFilterCommand.java
+++ src/main/java/org/thesandbox/core/commands/misc/chatFilterCommand.java
@@ -28,9 +28,10 @@ public class chatFilterCommand implements ISubCommand {
 
 
         boolean current = playerDataListener.get(player.getUniqueId(), PlayerDataKeys.CHATFILTER, false);
-        playerDataListener.set(player.getUniqueId(), PlayerDataKeys.CHATFILTER, !current);
-        player.sendMessage(Component.text("ChatFilter is now: " + current, NamedTextColor.YELLOW));
-        return false;
+        boolean newValue = !current;
+        playerDataListener.set(player.getUniqueId(), PlayerDataKeys.CHATFILTER, newValue);
+        player.sendMessage(Component.text("ChatFilter is now: " + newValue, NamedTextColor.YELLOW));
+        return true;
     }
 
     @Override
